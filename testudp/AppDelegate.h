dca2446880216be337d15dc7ec25f1ce3a611ad0
weigou <weigou@weigoudeMacBook-Pro.local>
2015-07-08 10:04:43 +0800

    Initial Commit

---
+++ testudp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testudp
+//
+//  Created by weigou on 15/7/8.
+//  Copyright (c) 2015年 weigou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testudp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testudp
+//
+//  Created by weigou on 15/7/8.
+//  Copyright (c) 2015年 weigou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
